9d7ef920688393405c6b3cdb5a3b2452477e2e4f
MOTSECK <motseck07@gmail.com>
2025-07-20 23:12:47 +0100

    ajout

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import '../styles/globals.css'; 
 import '../styles/header.css';
@@ -8,8 +8,8 @@ import '../styles/footer.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <App /> {/* App ne contient plus de <Router> */}
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
